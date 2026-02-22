cf44d0d59478cdac253bcf698fa9cb44e9add648
Adwiteek <sadwiteek@gmail.com>
2026-02-22 14:43:22 +0530

    database connection

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
-import enum
-from tkinter import NO
-from turtle import title
-from typing import Optional
 
-from fastapi import Body, FastAPI, HTTPException, Response,status
+from email.policy import default
+from xmlrpc.client import boolean
+
+from fastapi import  FastAPI, HTTPException, Response,status
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
 
 app=FastAPI()
 
 class Post(BaseModel):
     title:str
     content:str
-    id:int
+    published:boolean=True
+
+
+while(True):
+    try:
+        conn=psycopg2.connect(host='localhost',database='fastapi_practice',user='postgres',password='postgres123',cursor_factory=RealDictCursor)
+        cursor=conn.cursor()
+        print("Database connection is successful")
+        break
+    except Exception as error:
+        print("Failed to connect to database")
+        print("error: ", error)
+        time.sleep(2)
+    
+
 
 
 my_posts=[{"title":"Post1","content":"THis is post1","id":1},{"title":"Post2","content":"This is post2","id":2}]
 
-print(my_posts)
 
 def find_post(id):
     for p in my_posts:
@@ -30,14 +46,16 @@ async def root():
   
 @app.get("/posts")
 async def posts():
-    return {"data":my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts=cursor.fetchall()
+    return {"data":posts}
 
 @app.post("/createpost",status_code=status.HTTP_201_CREATED)
-async def createpost(new_post: Post):
-    print(new_post)
-    my_posts.append(new_post.dict())
-    print(my_posts)
-    return new_post
+async def createpost(post: Post):
+    cursor.execute("""INSERT INTO posts (title,content) VALUES (%s,%s) RETURNING * """,(post.title,post.content))
+    new_post=cursor.fetchone()
+    conn.commit()
+    return {"data":new_post} 
 
 # @app.get("/posts/{id}")
 # async def getpost(id:int):
@@ -51,13 +69,13 @@ async def getlatestpost():
 
 @app.get("/posts/{id}")
 async def getpost(id:int, response: Response):
-    post=find_post((id))
-    print(post)
+    cursor.execute("""SELECT * FROM posts WHERE id=%s""",(str(id),)) #the comma after str(id) needs to be studied here and str should be added because of type checking 
+    post=cursor.fetchone()
     if not post:
         # response.status_code=status.HTTP_404_NOT_FOUND           (can be done like this but there's a better way)
         # return {"message":f"post with id {id} not found"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found") #better way
-    return {"post":post}  #this is where "/posts/{id}" should be after "/posts/latest"
+    return {"post_detail":post}  #this is where "/posts/{id}" should be after "/posts/latest"
 
 
 def find_index_post(id):
@@ -68,18 +86,19 @@ def find_index_post(id):
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
 async def deletepost(id:int):
-    index=find_index_post(id) 
-    if index is None:
+    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING * """,(str(id),))
+    deleted_post=cursor.fetchone()
+    conn.commit()
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found")
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 async def updatepost(id:int,post:Post):
-    index=find_index_post(id) 
-    if index is None:
+    cursor.execute("""UPDATE posts SET title=%s, content=%s WHERE id=%s RETURNING * """,(post.title,post.content,str(id)))
+    updated_post=cursor.fetchone()
+    conn.commit()
+    if updated_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found")
-    post_dict=post.model_dump()
-    my_posts[index]=post_dict
-    return {"data":post}
+    return {"data":updated_post}
 
